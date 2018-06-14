eec299c1a979e2871d5e0b5255e81c5177ab3f66
Unknown <contact@okom3pom.com>
2018-06-14 14:33:21 +0200

    Clean

--- controllers/front/default.php
+++ controllers/front/default.php
@@ -42,49 +42,40 @@ class okom_moreinfoDefaultModuleFrontController extends ModuleFrontController
         $this->addCSS(_MODULE_DIR_.$module.'/views/css/okom_moreinfo.css');
         $this->context->controller->addJS(_PS_JS_DIR_.'validate.js');
     }
-    
+
     public function initContent()
     {
         parent::initContent();
-        
+
         $module_instance = new okom_moreinfo();
         if (Tools::isSubmit('sendEmail')) {
             $return = array();
-            
+
             $moreinfo_firstname = strval(Tools::getValue('moreinfo_firstname'));
             if (!$moreinfo_firstname  || empty($moreinfo_firstname) || !Validate::isName($moreinfo_firstname)) {
                 $return['errors'][] = $module_instance->l('Invalid firstname', 'default');
             }
-            
+
             $moreinfo_email = strval(Tools::getValue('moreinfo_email'));
             if (!Validate::isEmail($moreinfo_email)) {
                 $return['errors'][] = $module_instance->l('Invalid email', 'default');
             }
-                
-        
+
+
             $moreinfo_question = strval(Tools::getValue('moreinfo_question'));
             if (!$moreinfo_question  || empty($moreinfo_question) || !Validate::isMessage($moreinfo_question)) {
                 $return['errors'][] = $module_instance->l('Invalid question', 'default');
             }
-                
+
             $moreinfo_product = strval(Tools::getValue('moreinfo_product'));
             if (!$moreinfo_product  || empty($moreinfo_product) || !Validate::isUnsignedId($moreinfo_product)) {
                 $return['errors'][] = $module_instance->l('Invalid product id', 'default');
             }
 
-            /*
-            $moreinfo_consent = (int)Tools::getValue('consent');
-            if (!$moreinfo_consent  || empty($moreinfo_consent) || $moreinfo_consent != 1) {
-                $return['errors'][] = $module_instance->l('Vous devez donner votre accord', 'default');
-            }
-            */
-            
-        
-
             //Check if Captcha is enabled
             if (Configuration::get('OKOM_MOREINFO_CAPTCHA') == 1) {
                 $moreinfo_captcha = strval(Tools::getValue('ct_captcha'));
-                
+
                 if (!class_exists('Securimage')) {
                     require_once dirname(__FILE__) . '/../../securimage/securimage.php';
                 }
@@ -94,7 +85,7 @@ class okom_moreinfoDefaultModuleFrontController extends ModuleFrontController
                     $return['errors'][] = $module_instance->l('Incorrect security code entered', 'default');
                 }
             }
-            
+
             $id_lang = (int)$this->context->language->id;
             $iso = Language::getIsoById($id_lang);
             $product = new Product((int)$moreinfo_product, false, $id_lang);
@@ -104,7 +95,7 @@ class okom_moreinfoDefaultModuleFrontController extends ModuleFrontController
 
             if (!isset($return['errors'])) {
                 $link = new Link();
-                
+
                 $templateVars = array(
                     '{product}' => (is_array($product->name) ? $product->name[$id_lang] : $product->name),
                     '{product_link}' => $link->getProductLink($product),
@@ -112,7 +103,7 @@ class okom_moreinfoDefaultModuleFrontController extends ModuleFrontController
                     '{email}' => $moreinfo_email,
                     '{message}' => Tools::nl2br($moreinfo_question)
                 );
-                
+
                 if (file_exists(_PS_MODULE_DIR_.$this->module->name.'/mails/'.$iso.'/moreinfo.txt') && file_exists(_PS_MODULE_DIR_.$this->module->name.'/mails/'.$iso.'/moreinfo.html')) {
                     if (!Mail::Send(
                         (int)Configuration::get('PS_LANG_DEFAULT'),
@@ -142,33 +133,33 @@ class okom_moreinfoDefaultModuleFrontController extends ModuleFrontController
 
                 $question->save();
             }
-            
+
             $succes = '<div class="alert alert-success">'.$this->module->l('Question send with success.').'</div>';
-            
+
             $return['success'] = (isset($return['errors'])) ? false : $succes;
-            
+
             die(json_encode($return));
         } else {
             $module = 'okom_moreinfo';
-            
-                
+
+
             $faq_link = false;
             if (Module::isEnabled('faq')  && Configuration::get('OKOM_MOREINFO_FAQ') == 1) {
                 $url = (Configuration::get("PS_REWRITING_SETTINGS") == 1 && $this->getMeta()!= false) ? $this->getMeta():'index.php?fc=module&module=faq&controller=default';
                 $faq_link =  __PS_BASE_URI__.$url;
             }
-                
-                
+
+
             $phone_number = Configuration::get('OKOM_MOREINFO_TEL');
             $schedule = Configuration::get('OKOM_MOREINFO_TELH', (int)$this->context->language->id);
-                
+
             $secure_image = false;
-                
+
             if (Configuration::get('OKOM_MOREINFO_CAPTCHA') == 1) {
                 if (!class_exists('Securimage')) {
                     require_once dirname(__FILE__) . '/../../securimage/securimage.php';
                 }
-                    
+
                 $options = array();
                 $options['image_id'] = 'moreinfo_captcha';
                 $options['input_name'] = 'ct_captcha';
@@ -178,8 +169,8 @@ class okom_moreinfoDefaultModuleFrontController extends ModuleFrontController
             }
 
             $question_product = new Product((int)Tools::getValue('id_product'), 1, 1);
-                
-        
+
+
             $this->context->smarty->assign(array(
                     'nobots' => true,
                     'nofollow' => true,
@@ -195,7 +186,7 @@ class okom_moreinfoDefaultModuleFrontController extends ModuleFrontController
                     'fileupload' => Configuration::get('PS_CUSTOMER_SERVICE_FILE_UPLOAD'),
                     'customer' => $this->context->customer
                 ));
-                
+
 
             $this->setTemplate('productfooter.tpl');
         }
@@ -204,14 +195,14 @@ class okom_moreinfoDefaultModuleFrontController extends ModuleFrontController
     private function getMeta()
     {
         global  $cookie;
-        
+
         if (!$meta = Db::getInstance()->ExecuteS('SELECT `id_meta` FROM '._DB_PREFIX_.'meta WHERE `page`= \'module-faq-default\' ')) {
             return false;
         }
         if (!$metaUrlLang = Db::getInstance()->ExecuteS('SELECT `url_rewrite` FROM '._DB_PREFIX_.'meta_lang WHERE `id_meta`= '.$meta[0]['id_meta'].' AND `id_lang` = '.$cookie->id_lang.' AND `id_shop` = '.$this->context->shop->id)) {
             return false;
         }
-            
+
         return $metaUrlLang[0]['url_rewrite'];
     }
 }
